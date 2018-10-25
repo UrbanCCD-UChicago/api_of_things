d676b83290a5d186227c94d08a1030fe70918905
Jesus Bracho <jbrach347@gmail.com>
2018-10-25 11:38:56 -0400

    Check in, need to catch :eq, :lt, etc. query params

--- lib/aot_web/resolvers.ex
+++ lib/aot_web/resolvers.ex
@@ -1,17 +1,52 @@
 defmodule AotWeb.Resolvers do
-  def list_projects(_, _, _) do
-    {:ok, Aot.ProjectActions.list()}
+
+  @doc """
+  Example of possible `args` value:
+
+      iex> %{
+      ...>   name: %{
+      ...>     eq: "Chicago"
+      ...>   }
+      ...> }
+
+  """
+  def list_projects(_, args, _) do
+    {:ok, args
+      |> format_args()
+      |> Aot.ProjectActions.list()}
   end
 
-  def list_nodes(_, _, _) do
-    {:ok, Aot.NodeActions.list()}
+  def list_nodes(_, args, _) do
+    {:ok, args
+      |> format_args()
+      |> Aot.NodeActions.list()}
   end
 
-  def list_sensors(_, _, _) do
-    {:ok, Aot.SensorActions.list()}
+  def list_sensors(_, args, _) do
+    {:ok, args
+      |> format_args()
+      |> Aot.SensorActions.list()}
   end
 
-  def list_observations(_, _, _) do
-    {:ok, Aot.ObservationActions.list()}
+  def list_observations(_, args, _) do
+    {:ok, args
+      |> format_args()
+      |> Aot.ObservationActions.list()}
+  end
+
+  @doc """
+  It's the job of this function to convert `args` from a map to a list of tuples
+  that follow this structure:
+
+      iex> {:property, {:operator, "value"}}
+
+  """
+  defp format_args(args) do
+    args
+    |> Map.to_list()
+    |> Enum.map(fn {column, comparison} ->
+      [{operator, operand} | _] = Map.to_list(comparison)
+      {column, {operator, operand}}
+    end)
   end
 end
---
+++ test/aot_web/graphql_test.exs
@@ -0,0 +1,5 @@
+defmodule AotWeb.Testing.GraphqlTest do
+  use Aot.Testing.BaseCase
+  use Aot.Testing.DataCase
+  use AotWeb.Testing.ConnCase
+end
