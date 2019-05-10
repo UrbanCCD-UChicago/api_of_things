57eeff247042812fd21896fd02d4cc4be69faa69
Vince Forgione <vforgione@uchicago.edu>
2019-05-10 14:07:54 -0500

    Bump Version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Aot.MixProject do
   def project do
     [
       app: :aot,
-      version: "2.2.2",
+      version: "2.3.0",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
